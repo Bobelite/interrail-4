4f24dd3ed264c36dce51d1c52b47e7120dea17d8
Bobelite <Robert_darnell@interrail-signal.com>
2026-04-20 19:17:02 -0500

    Create Logo.tsx

---
+++ components/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div style={{ textAlign: 'center', marginBottom: 24 }}>
+      <div style={{ fontSize: 32, fontWeight: 800, color: '#0b63b6' }}>Interrail</div>
+      <div style={{ fontSize: 18, fontWeight: 700, color: '#4b5563' }}>Fleet</div>
+    </div>
+  );
+}
